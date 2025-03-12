9526e3310e94d65b6a5a072f6d16633248104c5b
Sai Suraj Suravajhala <suravajhala.s@northeastern.edu>
2025-03-12 16:16:24 -0400

    Updating Lambda code

--- spotify_api_data_extract.py
+++ spotify_api_data_extract.py
@@ -7,11 +7,11 @@ from datetime import datetime
 
 def lambda_handler(event, context):
     
-    cilent_id = os.environ.get('client_id')
+    client_id = os.environ.get('client_id')
     client_secret = os.environ.get('client_secret')
     
-    client_credentials_manager = SpotifyClientCredentials(client_id=cilent_id, client_secret=client_secret)
-    sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+    client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
     playlists = sp.user_playlists('spotify')
     
     playlist_link = "https://open.spotify.com/playlist/37i9dQZEVXbNG2KDcFcKOF?si=1333723a6eff4b7f"
@@ -19,11 +19,11 @@ def lambda_handler(event, context):
     
     spotify_data = sp.playlist_tracks(playlist_URI)   
     
-    cilent = boto3.client('s3')
+    client = boto3.client('s3')
     
     filename = "spotify_raw_" + str(datetime.now()) + ".json"
     
-    cilent.put_object(
+    client.put_object(
         Bucket="spotify-etl-project-darshil",
         Key="raw_data/to_processed/" + filename,
         Body=json.dumps(spotify_data)
